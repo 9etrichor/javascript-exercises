63a2ec99543aac3d20e1ee5a5986dc4e6905e868
9etrichor <dick2452683247@gmail.com>
2022-03-20 11:03:49 +0800

    finish task 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let sentence = '';
+    if(num >= 0)
+    {
+        for(i = 0;i < num; i = i + 1)
+        {
+            sentence = sentence + string;
+        }
+        return sentence;
+    }
+    else
+    {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
