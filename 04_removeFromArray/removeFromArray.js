59852540f0fa38f2606ec2a27810c29a2b457b4d
9etrichor <dick2452683247@gmail.com>
2022-03-20 12:03:04 +0800

    finish task 4, use array.filter(), function of checking same

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, e1, e2 ,e3 ,e4) {
+    function checkSame(arrayElement) {
+        return arrayElement !== e1 && arrayElement !== e2 && arrayElement !== e3 && arrayElement !== e4;
+    }
+    return array.filter(checkSame);
 };
 
 // Do not edit below this line
