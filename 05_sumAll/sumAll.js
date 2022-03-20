3fef142d532ae85bf7aec717bef93fe7073befc9
9etrichor <dick2452683247@gmail.com>
2022-03-20 12:34:17 +0800

    finish task5, use typeof check input type, use for loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let summation = 0;
+    if(typeof num1 === "number" && typeof num2 === "number" && num1 >= 0 && num2 >= 0)
+    {
+        if(num1 === num2)
+        {
+            return num1;
+        }
 
+        if(num1 > num2)
+        {
+            for(;num2 <= num1; num2 = num2 + 1)
+            {
+                summation = summation + num2;
+            }
+            return summation;
+        }
+
+        if(num1 < num2)
+        {
+            for(;num1 <= num2; num1 = num1 + 1)
+            {
+                summation = summation + num1;
+            }
+            return summation;
+        }
+    }
+    else
+    {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
